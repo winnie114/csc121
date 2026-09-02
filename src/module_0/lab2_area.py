34839541a643ae44368db40036caf9fd92d6123c
winnie114 <wmmwayi095@access.alamancecc.edu>
2026-09-01 23:39:55 -0400

    lab1_clock

--- src/module_0/lab2_area.py
+++ src/module_0/lab2_area.py
@@ -1,1 +1,13 @@
 # Refer to this module's readme
+def area(length, width):
+    print(str(length * width)) + " square feet"
+    return length * width 
+
+
+def main():
+    house = area(50, 20)
+    yard = area(50, 50)
+    total = house + yard
+    print (str(total)) + " total square feet"
+
+main()
--- src/module_0/lab3_machine.py
+++ src/module_0/lab3_machine.py
@@ -1,1 +1,14 @@
 # Refer to this module's readme
+emoticon = "v.v"
+
+def main():
+    global emoticon
+    say("Is anyone there?")
+    emoticon = "=D"
+    say("Oh, hi!")
+
+
+def say(phrase):
+    print(phrase + " " + emoticon)
+
+main()
