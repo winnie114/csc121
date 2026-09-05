35db01062441511b7b9ed34c9f15ec6557f0378f
winnie114 <wmmwayi095@access.alamancecc.edu>
2026-09-05 13:22:56 -0400

    module_0 labs2&3

--- src/module_0/lab2_area.py
+++ src/module_0/lab2_area.py
@@ -1,13 +1,10 @@
 # Refer to this module's readme
 def area(length, width):
-    print(str(length * width)) + " square feet"
     return length * width 
-
 
 def main():
     house = area(50, 20)
     yard = area(50, 50)
     total = house + yard
-    print (str(total)) + " total square feet"
-
+    print(str(total) + " square feet")
 main()
